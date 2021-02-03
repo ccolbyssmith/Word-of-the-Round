a937be2ac3799e9023686a2ca30993860b950232
ccolbyssmith <46413250+ccolbyssmith@users.noreply.github.com>
2021-02-03 13:02:56 -0800

    Buttons do something

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,35 +1,19 @@
 import os
 
 from flask import Flask
+from flask_socketio import SocketIO
+
+socketio = SocketIO()
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+def create_app(debug=False):
+    """Create an application."""
+    app = Flask(__name__)
+    app.debug = debug
+    app.config['SECRET_KEY'] = 'gjr39dkjn344_!67#'
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    from .views import views as views
+    app.register_blueprint(views)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # displays hello world
-    @app.route('/hello')
-    def hello():
-        return 'hello world!'
-
-    from . import view
-    app.register_blueprint(view.bp)
-
+    socketio.init_app(app)
     return app
---
+++ game.py
@@ -0,0 +1,6 @@
+from flaskr import create_app, socketio
+
+app = create_app(debug=True)
+
+if __name__ == '__main__':
+    socketio.run(app)
