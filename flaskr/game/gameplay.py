81d3f8f7946495fa8538324cd1177ad6a74db054
ccolbyssmith <46413250+ccolbyssmith@users.noreply.github.com>
2021-04-22 15:56:29 -0700

    Update gameplay.py

--- flaskr/game/gameplay.py
+++ flaskr/game/gameplay.py
@@ -2,10 +2,10 @@ class PromptDeck:
 	fileLocation = 'flaskr/data/prompts.json'
 
 	def __init__(self):
-		
+		self.createDeck()
 
 
-	def loadDeck(self):
+	def createDeck(self):
 		self.deck = []
 		with open(self.fileLocation, 'r') as read_file:
 			lines = read_file.readlines()
@@ -17,8 +17,9 @@ class WordDeck:
 	fileLocation = 'flaskr/data/words.json'
 
 	def __init__(self):
+		self.createDeck()
 
-	def loadDeck(self):
+	def createDeck(self):
 		self.deck = []
 		with open(self.fileLocation, 'r') as read_file:
 			lines = read_file.readlines()
